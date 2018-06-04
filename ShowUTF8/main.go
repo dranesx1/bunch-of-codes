970832d296494f783950cca03dbbd34307b0c8d9
dranesx1 <39699936+dranesx1@users.noreply.github.com>
2018-06-04 16:06:36 +0700

    Tabbing //comment

--- ShowUTF8/main.go
+++ ShowUTF8/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 200; i++ {						//i = init ; loop syntax for showing first 200 character UTF-8
+	for i := 0; i < 200; i++ {	//i = init ; loop syntax for showing first 200 character UTF-8
 		fmt.Printf("%d \t %q \n", i, i)	//%d decimal ; %q showing single quote character
 	}
 }
