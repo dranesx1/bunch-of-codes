7e2b88bd1b8ecc61622b4e3def3179628969a43e
dranesx1 <39699936+dranesx1@users.noreply.github.com>
2018-06-05 14:18:58 +0700

    Exercism.io gigasecond
    Adding gigasecond solution.
    
    All files fetched from exercism.io

---
+++ gigasecond/gigasecond.go
@@ -0,0 +1,16 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package gigasecond should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package gigasecond
+
+// import path for the time package from the standard library
+import "time"
+
+const gigasecond = time.Duration(1000000000) * time.Second
+
+// AddGigasecond using const to add duration 10^9 second
+func AddGigasecond(t time.Time) time.Time {
+	return t.Add(gigasecond) //returning the value to t.add(gigasecond) to showing final result
+}
